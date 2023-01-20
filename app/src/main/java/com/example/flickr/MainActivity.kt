ff4b19b9c82a25802da131857e066d6a586cfe71
Yahya Hassani <yahyamh01@gmail.com>
2023-01-20 22:34:28 +0100

    Add application class and config hilt

--- app/src/main/java/com/example/flickr/MainActivity.kt
+++ app/src/main/java/com/example/flickr/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.flickr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {  }
     }
 }
