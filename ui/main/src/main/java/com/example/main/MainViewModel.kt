e8e4a5d3bfd2b7552bb4b97be511049ef48832b8
Yahya Hassani <yahyamh01@gmail.com>
2023-01-30 11:03:17 +0100

    Improve ConnectivityStatusView.kt

--- ui/main/src/main/java/com/example/main/MainViewModel.kt
+++ ui/main/src/main/java/com/example/main/MainViewModel.kt
@@ -2,56 +2,51 @@ package com.example.main
 
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavHostController
+import com.example.main.MainUiState.ConnectivityStatus
+import com.example.main.MainUiState.Start
 import com.example.ui.common.BaseViewModel
 import com.example.ui.common.UIEvent
 import com.example.ui.common.UIState
 import com.example.ui.common.connectivity.ConnectivityMonitor
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.mapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val connectivityMonitor: ConnectivityMonitor,
     val navController: NavHostController,
-) : BaseViewModel<MainUiState, MainUiEvent>(MainUiState.HideNetStatusView) {
+) : BaseViewModel<MainUiState, MainUiEvent>(Start) {
     private var isAppLaunchedForFirstTime: Boolean = true
 
     init {
         observeConnectivityState()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     private fun observeConnectivityState() {
         connectivityMonitor.isOnline
             .distinctUntilChanged()
-            .onEach { isOnline ->
-                if (isOnline) {
-                    handelOnlineState()
-                } else {
-                    setState(MainUiState.Offline)
-                }
+            .mapLatest { isOnline ->
+                val shouldShowNetStatus = checkConnectivityStatusVisibility(isOnline)
+                setState(
+                    ConnectivityStatus(
+                        state.copy(
+                            isOnline = isOnline,
+                            isStatusVisible = shouldShowNetStatus
+                        )
+                    )
+                )
             }.launchIn(viewModelScope)
     }
 
-    private fun handelOnlineState() {
-        if (isAppLaunchedForFirstTime) {
-            isAppLaunchedForFirstTime = false
-            return
-        }
-        setState(MainUiState.Online)
-        hideOnlineViewAfterWhile()
-    }
-
-    private fun hideOnlineViewAfterWhile() {
-        val hideOnlineViewDelay: Long = 2000
-        viewModelScope.launch {
-            delay(hideOnlineViewDelay)
-            setState(MainUiState.HideNetStatusView)
-        }
+    private fun checkConnectivityStatusVisibility(isOnline: Boolean): Boolean {
+        val shouldShowNetStatus = (isAppLaunchedForFirstTime && isOnline).not()
+        isAppLaunchedForFirstTime = false
+        return shouldShowNetStatus
     }
 
     override fun onEvent(event: MainUiEvent) {}
@@ -59,16 +54,24 @@ class MainViewModel @Inject constructor(
 
 sealed interface MainUiEvent : UIEvent
 
-sealed class MainUiState(
-    val isOnlineViewVisible: Boolean = false,
-    val isOfflineViewVisible: Boolean = false,
+open class MainUiState(
+    val isOnline: Boolean = false,
+    val isConnectivityStatusVisible: Boolean = false,
 ) : UIState {
-    object HideNetStatusView : MainUiState(
-        isOnlineViewVisible = false,
-        isOfflineViewVisible = false,
+    constructor(state: MainUiState) : this(
+        state.isOnline,
+        state.isConnectivityStatusVisible
     )
 
-    object Offline : MainUiState(isOfflineViewVisible = true)
-    object Online : MainUiState(isOnlineViewVisible = true)
+    fun copy(
+        isOnline: Boolean = this.isOnline,
+        isStatusVisible: Boolean = this.isConnectivityStatusVisible,
+    ) = MainUiState(
+        isOnline = isOnline,
+        isConnectivityStatusVisible = isStatusVisible,
+    )
+
+    object Start : MainUiState()
+    class ConnectivityStatus(state: MainUiState) : MainUiState(state)
 }
 
